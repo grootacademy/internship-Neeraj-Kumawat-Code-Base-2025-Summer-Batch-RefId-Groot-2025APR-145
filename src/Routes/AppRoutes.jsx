13d0a617df7baeaf75c856f18b7770d1edc6b766
neeraj-717 <neerajkumawat717@gmail.com>
2025-11-04 16:44:11 +0530

    commu

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+  // import React from "react";
 // import { Routes, Route } from "react-router-dom";
 // import Dashbord from "../pages/Dashbord";
 // import Courses from "../pages/courses/Courses";
